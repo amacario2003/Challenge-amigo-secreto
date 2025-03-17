306951787a9457e5b1f182ae0615b17a153c8702
amacario2003 <mjosvin@gmail.com>
2025-03-17 11:08:53 -0600

    Este es el segundo push

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Challenge-amigo-secreto
 Este es un proyecto hecho con tecnologías de desarrollo web, tales como HTML, CSS y JavaScript, con la finalidad de reforzar la lógica de promocionan aprendida a lo largo de los cursos de Alura Latam
 Este es el primer push hacia el repositorio con los recusos necesarios para comenzar con el desarrollo del proyecto. 
+
+Este es el segundo push hacia el repositrio con las primeras variables y 'function' creadas, esto con el fin de poder agregar amigos al array y limpiar el campo una vez se haya hecho el push.
--- app.js
+++ app.js
@@ -1,1 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombreAmigo;
+let listaAmigos = [];
+
+function asignarTexto(elemento, texto){
+    let elementoHTML = document.getElementById(elemento)
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function limpiarInput(){
+    document.querySelector('#amigo').value ='';
+}
+
+function agregarAmigo(){
+
+    nombreAmigo = document.getElementById('amigo').value;
+
+    if(nombreAmigo === ''){
+        alert("Por favor, inserte un nombre.");
+    }else{
+        listaAmigos.push(nombreAmigo);
+        console.log("este es el nombre insertado hasta el momento "+nombreAmigo);
+        console.log("estos son los items en el array "+listaAmigos);
+        limpiarInput();
+    }
+   
+
+}
